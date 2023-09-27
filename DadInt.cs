3cb7db8c3d455bd1750c61c69577a5bc06b90067
Francisco Texeira <fc56305@alunos.fc.ul.pt>
2023-09-27 15:28:40 +0100

    DadInt created

---
+++ DadInt.cs
@@ -0,0 +1,14 @@
+class DadInt {
+    private int pairValue;
+    private string pairKey;
+    public string key {
+        get { return pairKey; }
+        set { pairKey = value; }
+    }
+
+    public int value {
+        get { return pairValue; }
+        set { pairValue = value; }
+    }
+
+}
